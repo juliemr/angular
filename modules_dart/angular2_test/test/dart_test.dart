0d1142883be415401c5a3605dd696651c9910e91
Kevin Moore <kevmoo@google.com>
2015-07-17 16:16:43 -0700

    cleanup imports

--- modules_dart/angular2_test/test/dart_test.dart
+++ modules_dart/angular2_test/test/dart_test.dart
@@ -1,14 +1,17 @@
 // Because Angular is using dart:html, we need these tests to run on an actual
 // browser. This means that it should be run with `-p dartium` or `-p chrome`.
 @TestOn("browser")
-import "package:test/test.dart"; // Instead, import angular test lib?
 import "package:angular2/angular2.dart"
     show Component, View, NgFor, Injector, Key;
-import "package:angular2/test.dart"
-    show TestComponentBuilder, inject, createTestInjector;
+
 import 'package:angular2/src/dom/browser_adapter.dart'; // for BrowserDomAdapter
 import 'package:angular2/src/reflection/reflection.dart'; // for reflector
 import 'package:angular2/src/reflection/reflection_capabilities.dart'; // For ReflectionCapabilities
+
+import 'package:angular2/src/test_lib/test_component_builder.dart';
+import 'package:angular2/src/test_lib/test_injector.dart';
+
+import "package:test/test.dart"; // Instead, import angular test lib?
 
 // This is the component we will be testing.
 @Component(selector: 'test-cmp')
@@ -20,51 +23,40 @@ class TestComponent {
   }
 }
 
+const TEMPLATE =
+    "<div><copy-me template=\"ng-for #item of items\">{{item.toString()}};</copy-me></div>";
+
 void main() {
-  test("normal, synchronous test", () {
-    var string = "foo,bar,baz";
-    expect(string.split(","), equals(["foo", "bar", "baz"]));
-  });
+  Injector testInjector;
 
-  var TEMPLATE =
-      "<div><copy-me template=\"ng-for #item of items\">{{item.toString()}};</copy-me></div>";
-
-  test("create a component using the TCB", () {
+  setUp(() {
     BrowserDomAdapter.makeCurrent();
     reflector.reflectionCapabilities = new ReflectionCapabilities();
 
-    Injector testInjector = createTestInjector([]);
+    testInjector = createTestInjector([]);
+  });
 
-    inject([TestComponentBuilder], (TestComponentBuilder tcb) {
-      var future = tcb
+  test("create a component using the TCB", () async {
+    await inject([TestComponentBuilder], (TestComponentBuilder tcb) async {
+      var rootTC = await tcb
           .overrideTemplate(TestComponent, TEMPLATE)
-          .createAsync(TestComponent)
-          .then((rootTC) {
-        rootTC.detectChanges();
-        expect(rootTC.nativeElement.text, equals("1;2;"));
-      });
+          .createAsync(TestComponent);
 
-      expect(future, completes);
+      rootTC.detectChanges();
+      expect(rootTC.nativeElement.text, equals("1;2;"));
     }).execute(testInjector);
   });
 
-  test("should reflect added elements", () {
-    BrowserDomAdapter.makeCurrent();
-    reflector.reflectionCapabilities = new ReflectionCapabilities();
-
-    Injector testInjector = createTestInjector([]);
-
-    inject([TestComponentBuilder], (TestComponentBuilder tcb) {
-      var future = tcb
+  test("should reflect added elements", () async {
+    await inject([TestComponentBuilder], (TestComponentBuilder tcb) async {
+      var rootTC = await tcb
           .overrideTemplate(TestComponent, TEMPLATE)
-          .createAsync(TestComponent)
-          .then((rootTC) {
-        rootTC.detectChanges();
-        ((rootTC.componentInstance.items as List<num>)).add(3);
-        rootTC.detectChanges();
-        expect(rootTC.nativeElement.text, equals("1;2;3;"));
-      });
-      expect(future, completes);
+          .createAsync(TestComponent);
+
+      rootTC.detectChanges();
+      ((rootTC.componentInstance.items as List<num>)).add(3);
+      rootTC.detectChanges();
+      expect(rootTC.nativeElement.text, equals("1;2;3;"));
     }).execute(testInjector);
   });
 }
